9414d88ecf1c13fa3fda96ef7f55834079311347
kcjennin <kylejennings5@gmail.com>
2023-03-03 17:47:04 -0800

    Reorganized the scenes and added a level
    The player and the world both have their own scenes now. These get
    linked into the main scene. That way the project is a bit more split up.
    Also made a few changes to the Player controller to make it better.

--- Main.cs
+++ Main.cs
@@ -3,31 +3,24 @@ using System;
 
 public partial class Main : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		Input.MouseMode = Input.MouseModeEnum.Captured;
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
 
     public override void _Input(InputEvent @event)
     {
         if (@event.IsActionPressed("ui_cancel"))
-		{
-			Input.MouseMode = Input.MouseModeEnum.Visible;
-		}
+        {
+            Input.MouseMode = Input.MouseModeEnum.Visible;
+        }
 
-		if (@event.IsActionPressed("click"))
-		{
-			if (Input.MouseMode == Input.MouseModeEnum.Visible)
-			{
-				Input.MouseMode = Input.MouseModeEnum.Captured;
-				GetTree().Quit();
-			}
-		}
+        if (@event.IsActionPressed("click") &&
+            Input.MouseMode == Input.MouseModeEnum.Visible)
+        {
+            Input.MouseMode = Input.MouseModeEnum.Captured;
+            GetViewport().SetInputAsHandled();
+        }
     }
 }
